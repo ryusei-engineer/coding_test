491a6b90c8fbadfec79bdf9ea2fd1af8cf1a4a77
ryusei-engineer <ryusei.engineer@gmail.com>
2024-06-23 23:31:15 +0900

    includs() Number() toSrting()

--- index.js
+++ index.js
@@ -40,3 +40,35 @@ for(let i = 1; i < lines[0]; i++){
     process.stdout.write("paiza ");
 }
 process.stdout.write("paiza");
+
+//C問題
+const lines = require("fs").readFileSync("/dev/stdin", "utf8").split("\n");
+
+for(let i = 1; i <= lines[0]; i++){
+    const array = lines[i].split(" ");
+    //Number()で文字から数値に変換
+    array[1] = Number(array[1]);
+    array[1]++;
+    console.log(array.join(" "));
+}
+
+// D問題
+const lines = require("fs").readFileSync("/dev/stdin", "utf8").split("\n");
+
+for(let i = 1; i <= lines[0]; i++){
+    let num = lines[i];
+    //toString()文字列に変換
+    //数値の桁数を取得するには文字列に変換後.lengthで桁数を取得
+    console.log(num.toString().length);
+}
+
+// D問題
+const lines = require("fs").readFileSync("/dev/stdin", "utf8").split("\n");
+
+let word = lines[0];
+//str.includes("a")でaがstrに含まれているか、trueかfalseを返す
+if(lines[1].includes(word)){
+    console.log("YES");
+}else{
+    console.log("NO");
+}
